5478887fc2c1ecc029728c862b255b1d42b64d6f
chihokyo <chihoukyo@gmail.com>
2020-08-13 23:24:28 +0900

    【webpack】自动化设置

--- webpack_demo/src/index.js
+++ webpack_demo/src/index.js
@@ -2,5 +2,5 @@ import $ from 'jquery'
 
 $(function () {
   $('li:odd').css('backgroundColor', 'pink')
-  $('li:even').css('backgroundColor', 'lightblue')
+  $('li:even').css('backgroundColor', 'green')
 })
