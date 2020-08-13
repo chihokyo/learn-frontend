506b43eef891c665cff3510a20394005f10a253d
chihokyo <chihoukyo@gmail.com>
2020-08-13 23:36:30 +0900

    【webpack】预览页面插件

--- webpack_demo/webpack.config.js
+++ webpack_demo/webpack.config.js
@@ -3,13 +3,22 @@
  * production : 生产环境 压缩
  * webpack4.0 默认打包入口文件 src -> index.js
  * 默认打包出口文件 dist -> main.js
- *
+ * 预览页面插件 html-webpack-plugin
  */
 
 // 导入nodejs里路径path模块
 const path = require('path')
+// 导入预览页面插件 得到构造函数
+const HtmlWebpakcPlugin = require('html-webpack-plugin')
 
+const htmlPlugin = new HtmlWebpakcPlugin(
+  {
+    template: './src/index.html',
+    filename: 'index.html'
+  }
+)
 module.exports = {
+  mode: 'development',
   // 入口文件夹
   entry: path.join(__dirname, './src/index.js'),
   output: {
@@ -18,7 +27,6 @@ module.exports = {
     // 出口文件名
     filename: 'bundle.js'
   },
-
-  mode: 'development'
+  // 预览页面配置节点
+  plugins: [htmlPlugin]
 }
-
